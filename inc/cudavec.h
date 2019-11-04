1b0f5bc973be5aa04a4d2e4a10f46a390cd1247b
Marijn Stollenga <mstollenga@gmail.com>
2019-11-04 23:14:33 +0100

    Rename n to N

--- inc/cudavec.h
+++ inc/cudavec.h
@@ -8,75 +8,72 @@
 template <typename F>
 struct CudaVec {
 	F *data = 0;
-	int n = 0;
+	int N = 0;
 
-	CudaVec() : data(0), n(0) { }
-	CudaVec(int n_) : data(0), n(0) { resize(n_); }
+	CudaVec() : data(0), N(0) { }
+	CudaVec(int n_) : data(0), N(0) { resize(n_); }
 	~CudaVec() {
-	  if (n) {
-	    std::cout <<"deallocating " << n << std::endl;
+	  if (N) {
 	    cudaFree(data);
 	  }
 	}	  
 	
 	void resize(int n2) {
-		if (n != n2) {
-          if (n) {
-            std::cout << "freeing " << n << std::endl;
+		if (N != n2) {
+          if (N) {
             cudaFree(data);
           }
-          std::cout <<"allocating " << n2 << std::endl;
           handle_error( cudaMalloc( (void**)&data, sizeof(F) * n2));
-          n = n2;
+          N = n2;
 		}
 		zero();
 	}
 
 	CudaVec(CudaVec &other) {
-      resize(other.n);  
-      copy_gpu_to_gpu(other.data, data, n);
+      resize(other.N);  
+      copy_gpu_to_gpu(other.data, data, N);
 	}
 
   CudaVec &operator=(CudaVec &other) {
-    if (n != other.n) {
-      resize(other.n);
+    if (N != other.N) {
+      resize(other.N);
     }
-    copy_gpu_to_gpu(other.data, data, n);
+    copy_gpu_to_gpu(other.data, data, N);
     return *this;
   }
     
 	void rand_zero(F p);
 
   void zero(int offset = 0) {
-		handle_error( cudaMemset(data + offset, 0, sizeof(F) * (n - offset) ) );
+		handle_error( cudaMemset(data + offset, 0, sizeof(F) * (N - offset) ) );
 	}
 
 	void init_normal(F mean, F std) {
-		::init_normal<F>(data, n, mean, std);
+		::init_normal<F>(data, N, mean, std);
 	}
 
   	void add_normal(F mean, F std) {
-		::add_normal<F>(data, n, mean, std);
+		::add_normal<F>(data, N, mean, std);
 	}
 
 	std::vector<F> to_vector() {
-		std::vector<F> vec(n);
-		handle_error( cudaMemcpy(&vec[0], data, n * sizeof(F), cudaMemcpyDeviceToHost));
+		std::vector<F> vec(N);
+		handle_error( cudaMemcpy(&vec[0], data, N * sizeof(F), cudaMemcpyDeviceToHost));
 		return vec;
 	}
 
 	void from_vector(std::vector<F> &vec) {
-		if (vec.size() != n)
+		if (vec.size() != N)
 			resize(vec.size());
-		handle_error( cudaMemcpy(data, &vec[0], n * sizeof(F), cudaMemcpyHostToDevice));
+		handle_error( cudaMemcpy(data, &vec[0], N * sizeof(F), cudaMemcpyHostToDevice));
 	}
 
   F sum() {
     F result(0);
     if (sizeof(F) == sizeof(float))
-	    handle_error( cublasSasum(Handler::cublas(), n, data, 1, &result) );
+	    handle_error( cublasSasum(Handler::cublas(), N, data, 1, &result) );
     else
-    	handle_error( cublasDasum(Handler::cublas(), n, data, 1, &result) );
+    	handle_error( cublasDasum(Handler::cublas(), N, data, 1, &result) );
     return result;
   }
   
