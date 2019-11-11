4c4a060059f6cc826d42e8ba0b894e3edd698f35
Marijn Stollenga <mstollenga@gmail.com>
2019-11-12 00:01:33 +0100

    train

---
+++ train.cc
@@ -0,0 +1,34 @@
+#include "network.h"
+
+#include "tensor.h"
+#include <vector>
+#include <thread>
+
+using namespace std;
+
+typedef function<void(vector<unique_ptr<Tensor<float>>>*)> GrabSamplesFunc;
+
+struct Trainer {
+    Trainer(GrabSamplesFunc func) : grab_samples_func(func) {}
+
+
+    void start() {
+        grab_samples_func(&samples);
+
+        while (true) {
+            thread t(grab_samples_func, &next_samples);
+            
+        }
+    }
+
+    vector<unique_ptr<Tensor<float>>> samples;
+    vector<unique_ptr<Tensor<float>>> next_samples;
+
+    GrabSamplesFunc grab_samples_func;
+};
+
+int main(int argc, char **argv) {
+    Network<float> network;
+
+    
+}
