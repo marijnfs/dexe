f33809f897f032d2ba2644ccae4fdebb115100c9
Marijn Stollenga <stollenga@imfusion.de>
2020-01-24 15:12:41 +0100

    Moving binaries, remove some output

--- inc/dexe/cudavec.h
+++ inc/dexe/cudavec.h
@@ -1,115 +1,110 @@
 #pragma once
 
-#include <vector>
-#include <cuda.h>
 #include "util.h"
+#include <cuda.h>
+#include <vector>
 
 namespace dexe {
 
-template <typename F>
-struct CudaVec {
-	F *data = 0;
-	int N = 0;
-	bool own = true;
+template <typename F> struct CudaVec {
+    F *data = 0;
+    int N = 0;
+    bool own = true;
 
-	CudaVec() : data(0), N(0) { }
-	CudaVec(int n_) : data(0), N(0) { resize(n_); }
-	CudaVec(F *data, int n_) : data(data), N(n_), own(false) {}
-	
-	~CudaVec() {
-	  if (own && N) {
+    CudaVec() : data(0), N(0) {}
+    CudaVec(int n_) : data(0), N(0) { resize(n_); }
+    CudaVec(F *data, int n_) : data(data), N(n_), own(false) {}
+
+    ~CudaVec() {
+	if (own && N) {
 	    cudaFree(data);
-	  }
-	}	  
-	
-	void resize(int n2) {
-		if (!own)
-			throw std::runtime_error("Can resize non-owning cudavec");
-
-		if (N != n2) {
-          if (N) {
-            cudaFree(data);
-			data = 0;
-          }
-		  if (n2)
-			  handle_error( cudaMalloc( (void**)&data, sizeof(F) * n2));
-          N = n2;
-		}
-		zero();
 	}
-
-	CudaVec(CudaVec &other) {
-      resize(other.N);  
-      copy_gpu_to_gpu(other.data, data, N);
-	}
-
-  CudaVec &operator=(CudaVec &other) {
-    if (N != other.N) {
-      resize(other.N);
     }
-    copy_gpu_to_gpu(other.data, data, N);
-    return *this;
-  }
-    
-	// void rand_zero(F p);
 
-  void zero(int offset = 0) {
-	  if (N)
-		  handle_error( cudaMemset(data + offset, 0, sizeof(F) * (N - offset) ) );
+    CudaVec &operator=(CudaVec &other) {
+	if (N != other.N) {
+	    resize(other.N);
 	}
+	copy_gpu_to_gpu(other.data, data, N);
+	return *this;
+    }
 
-	void init_normal(F mean, F std) {
-		dexe::init_normal<F>(data, N, mean, std);
+    void resize(int n2) {
+	if (!own)
+	    throw std::runtime_error("Can resize non-owning cudavec");
+
+	if (N != n2) {
+	    if (N) {
+		cudaFree(data);
+		data = 0;
+	    }
+	    if (n2)
+		handle_error(cudaMalloc((void **)&data, sizeof(F) * n2));
+	    N = n2;
 	}
+	zero();
+    }
 
-  	void add_normal(F mean, F std) {
-		dexe::add_normal<F>(data, N, mean, std);
-	}
+    CudaVec(CudaVec &other) {
+	resize(other.N);
+	copy_gpu_to_gpu(other.data, data, N);
+    }
 
-	std::vector<F> to_vector() {
-		std::vector<F> vec(N);
-		handle_error( cudaMemcpy(&vec[0], data, N * sizeof(F), cudaMemcpyDeviceToHost));
-		return vec;
-	}
+    // void rand_zero(F p);
 
-	void from_vector(std::vector<F> &vec) {
-		if (vec.size() != N)
-			resize(vec.size());
-		handle_error( cudaMemcpy(data, &vec[0], N * sizeof(F), cudaMemcpyHostToDevice));
-	}
+    void zero(int offset = 0) {
+	if (N)
+	    handle_error(
+		cudaMemset(data + offset, 0, sizeof(F) * (N - offset)));
+    }
 
-  F sum() {
-    F result(0);
-    if (sizeof(F) == sizeof(float))
-	    handle_error( cublasSasum(Handler::cublas(), N, data, 1, &result) );
-    else
-    	handle_error( cublasDasum(Handler::cublas(), N, data, 1, &result) );
-    return result;
-  }
-  
-  
-  
-	CudaVec<F> &sqrt();
-	CudaVec<F> &abs();
-	CudaVec<F> &pow(F e);
-	CudaVec<F> &exp();
-	CudaVec<F> &clip(F limit);
-	CudaVec<F> &add(int idx, F val);
+    void init_normal(F mean, F std) {
+	dexe::init_normal<F>(data, N, mean, std);
+    }
 
-	CudaVec<F> &operator-=(CudaVec<F> &other);
-	CudaVec<F> &operator+=(CudaVec<F> &other);
-	CudaVec<F> &operator*=(CudaVec<F> &other);
-	CudaVec<F> &operator/=(CudaVec<F> &other);
-	CudaVec<F> &operator/=(F val);
+    void add_normal(F mean, F std) { dexe::add_normal<F>(data, N, mean, std); }
 
+    std::vector<F> to_vector() {
+	std::vector<F> vec(N);
+	handle_error(
+	    cudaMemcpy(&vec[0], data, N * sizeof(F), cudaMemcpyDeviceToHost));
+	return vec;
+    }
 
-	CudaVec &operator*=(F v);
-	CudaVec &operator+=(F v);
+    void from_vector(std::vector<F> &vec) {
+	if (vec.size() != N)
+	    resize(vec.size());
+	handle_error(
+	    cudaMemcpy(data, &vec[0], N * sizeof(F), cudaMemcpyHostToDevice));
+    }
 
-	//Remove copy and assignment operator to be safe
-	CudaVec<F> & operator=(const CudaVec<F>&) = delete;
-    CudaVec<F>(const CudaVec<F>&) = delete;
+    F sum() {
+	F result(0);
+	if (sizeof(F) == sizeof(float))
+	    handle_error(cublasSasum(Handler::cublas(), N, data, 1, &result));
+	else
+	    handle_error(cublasDasum(Handler::cublas(), N, data, 1, &result));
+	return result;
+    }
 
+    CudaVec<F> &sqrt();
+    CudaVec<F> &abs();
+    CudaVec<F> &pow(F e);
+    CudaVec<F> &exp();
+    CudaVec<F> &clip(F limit);
+    CudaVec<F> &add(int idx, F val);
+
+    CudaVec<F> &operator-=(CudaVec<F> &other);
+    CudaVec<F> &operator+=(CudaVec<F> &other);
+    CudaVec<F> &operator*=(CudaVec<F> &other);
+    CudaVec<F> &operator/=(CudaVec<F> &other);
+    CudaVec<F> &operator/=(F val);
+
+    CudaVec &operator*=(F v);
+    CudaVec &operator+=(F v);
+
+    // Remove copy operator to be safe
+    CudaVec<F>(const CudaVec<F> &) = delete;
 };
 
 __global__ void sqrt_kernel(float *v, int n);
@@ -132,4 +127,4 @@ __global__ void abs_kerneld(double *v, int n);
 __global__ void exp_kerneld(double *v, int n);
 __global__ void clip_kerneld(double *v, int n, double limit);
 
-}
+} // namespace dexe
