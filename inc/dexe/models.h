b58ee62a09aedd2ca5c2e6a34883605758965e69
Marijn Stollenga <stollenga@imfusion.de>
2020-02-17 14:49:22 +0100

    added missing models.h

---
+++ inc/dexe/models.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "network.h"
+#include <functional>
+
+namespace dexe {
+
+    template <typename F>
+        std::function<Node<F>(Node<F>)> basic_layer(int c, int c_out, int k);
+    
+    template <typename F>
+        Node<F> make_unet(Network<F> *network, int in_channels, int out_channels);
+}
