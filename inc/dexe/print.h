6897e7c0abbc501054d0ff010c117837eaed2767
Marijn Stollenga <mstollenga@gmail.com>
2020-04-04 01:51:27 +0200

    add print.h

---
+++ inc/dexe/print.h
@@ -0,0 +1,26 @@
+#pragma once
+
+void print() {
+}
+
+template <typename T> void print(const T& t) {
+    std::cout << t << std::endl;
+}
+
+template <typename First, typename... Rest> void print(const First& first, const Rest&... rest) {
+    std::cout << first;
+    print(rest...); // recursive call using pack expansion syntax
+}
+
+void println() {
+    std::cout << std::endl;
+}
+
+template <typename T> void println(const T& t) {
+    std::cout << t << std::endl;
+}
+
+template <typename First, typename... Rest> void println(const First& first, const Rest&... rest) {
+    std::cout << first;
+    println(rest...); // recursive call using pack expansion syntax
+}
