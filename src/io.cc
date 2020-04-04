c415436e7b22fadb327ac84d3fc45120efb6b5ea
Marijn Stollenga <mstollenga@gmail.com>
2020-04-04 03:03:35 +0200

    editing

---
+++ src/io.cc
@@ -0,0 +1,39 @@
+#include "dexe/io.h"
+
+
+#include <nifti/nifti1_io.h>
+
+namespace dexe {
+
+std::unique_ptr<Tensor<float>> read_nifti(std::string &path) {
+    auto image = nifti_image_read(path.c_str(), 1);
+    if (!image)
+    	throw std::runtime_error("reading failed");
+    
+    auto dtype = image->datatype;
+    auto n_dim = image->dim[0];
+    auto bytesper = image->nbyper;
+    auto n_vox = image->nvox;
+
+    std::string description(image->descrip);
+    
+    std::vector<int> dims(n_dim + 2);
+    dims[0] = dims[1] = 1;
+    std::copy(&image->dim[1], &image->dim[1+n_dim], &dims[2]);
+
+    std::cout << dims << std::endl;
+    std::cout << description << std::endl;
+    std::cout << n_vox << " x " << bytesper << std::endl;
+    
+    std::vector<float> converted(n_vox);
+    if (bytesper == 2) {
+        uint16_t *data = (uint16_t*) image->data;
+        std::copy(data, data + n_vox, converted.begin());
+    }
+    auto tensor = std::make_unique<Tensor<float>>(TensorShape{dims});
+    tensor->from_vector(converted);
+    return tensor;
+}
+
+
+}
