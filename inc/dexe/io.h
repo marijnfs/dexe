134b94a7252da2e5f740dc855e27737477bdc992
Marijn Stollenga <mstollenga@gmail.com>
2020-04-05 20:53:44 +0200

    [Dexe] Added nifti writing

--- inc/dexe/io.h
+++ inc/dexe/io.h
@@ -5,7 +5,8 @@
 
 #ifdef USE_NIFTI
 namespace dexe {
-    std::unique_ptr<Tensor<float>> read_nifti(std::string &path);
+    std::unique_ptr<Tensor<float>> read_nifti(std::string path);
+	void write_nifti(std::string path, Tensor<float> &data);
 
 }
 #endif
--- src/io.cc
+++ src/io.cc
@@ -5,7 +5,7 @@
 
 namespace dexe {
 
-std::unique_ptr<Tensor<float>> read_nifti(std::string &path) {
+std::unique_ptr<Tensor<float>> read_nifti(std::string path) {
     auto image = nifti_image_read(path.c_str(), 1);
     if (!image)
     	throw std::runtime_error("reading failed");
@@ -18,7 +18,7 @@ std::unique_ptr<Tensor<float>> read_nifti(std::string &path) {
     std::string description(image->descrip);
     
     std::vector<int> dims(n_dim + 2);
-    dims[0] = dims[1] = 1;
+    dims[0] = dims[1] = 1; //right now single channel image is assumed
     std::copy(&image->dim[1], &image->dim[1+n_dim], &dims[2]);
 
     std::cout << dims << std::endl;
@@ -54,8 +54,46 @@ std::unique_ptr<Tensor<float>> read_nifti(std::string &path) {
 
     auto tensor = std::make_unique<Tensor<float>>(TensorShape{dims});
     tensor->from_vector(converted);
+
+    nifti_image_free(image);
     return tensor;
 }
 
 
+void write_nifti(std::string path, Tensor<float> &data) {
+    auto ni = nifti_simple_init_nim();
+    ni->datatype = NIFTI_TYPE_FLOAT32;
+    ni->nbyper = 4;
+
+    ni->fname = nifti_strdup(path.c_str());
+
+    auto &dimensions = data.shape.dimensions;
+    if (dimensions.size() < 3) {
+        throw std::runtime_error("unsupported number of dimensions");
+    }
+    if (dimensions[0] != 1 || dimensions[1] != 1) {
+        throw std::runtime_error("batch size and channel size must be one for writing nifti");
+    }
+
+    ni->ndim = dimensions.size();
+    ni->dim[0] = dimensions.size();
+    copy(dimensions.begin() + 2, dimensions.end(), &ni->dim[1]);
+
+    if (dimensions.size() > 2)
+        ni->nx = dimensions[2];
+    if (dimensions.size() > 3)
+        ni->ny = dimensions[3];
+    if (dimensions.size() > 4)
+        ni->nz = dimensions[4];
+
+    ni->nvox = data.shape.n_elements();
+
+    auto data_vec = data.to_vector();
+    ni->data = &data_vec[0];
+
+    nifti_image_write(ni);
+    ni->data = nullptr;
+    nifti_image_free(ni);
 }
+
+} //namespace
