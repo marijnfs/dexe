c4806c58175de14ab5aee944d97ec9c3fa0c96db
Marijn Stollenga <mstollenga@gmail.com>
2020-04-05 14:33:21 +0200

    Add nifti support for most datatypes

--- src/io.cc
+++ src/io.cc
@@ -23,13 +23,35 @@ std::unique_ptr<Tensor<float>> read_nifti(std::string &path) {
 
     std::cout << dims << std::endl;
     std::cout << description << std::endl;
-    std::cout << n_vox << " x " << bytesper << std::endl;
+    std::cout << n_vox << " x " << bytesper << " dtype: " << nifti_datatype_to_string(dtype) << std::endl;
     
     std::vector<float> converted(n_vox);
-    if (bytesper == 2) {
-        uint16_t *data = (uint16_t*) image->data;
-        std::copy(data, data + n_vox, converted.begin());
-    }
+
+    auto data = image->data;
+    if (dtype == NIFTI_TYPE_UINT8)
+        std::copy(reinterpret_cast<uint8_t*>(data), reinterpret_cast<uint8_t*>(data) + n_vox, converted.begin());
+    else if (dtype == NIFTI_TYPE_INT16)
+        std::copy(reinterpret_cast<int16_t*>(data), reinterpret_cast<int16_t*>(data) + n_vox, converted.begin());
+    else if (dtype == NIFTI_TYPE_INT32)
+        std::copy(reinterpret_cast<int32_t*>(data), reinterpret_cast<int32_t*>(data) + n_vox, converted.begin());
+    else if (dtype == NIFTI_TYPE_FLOAT32)
+        std::copy(reinterpret_cast<float*>(data), reinterpret_cast<float*>(data) + n_vox, converted.begin());
+    else if (dtype == NIFTI_TYPE_FLOAT64)
+        std::copy(reinterpret_cast<double*>(data), reinterpret_cast<double*>(data) + n_vox, converted.begin());
+    else if (dtype == NIFTI_TYPE_INT8)
+        std::copy(reinterpret_cast<int8_t*>(data), reinterpret_cast<int8_t*>(data) + n_vox, converted.begin());
+    else if (dtype == NIFTI_TYPE_UINT16)
+        std::copy(reinterpret_cast<uint16_t*>(data), reinterpret_cast<uint16_t*>(data) + n_vox, converted.begin());
+    else if (dtype == NIFTI_TYPE_UINT32)
+        std::copy(reinterpret_cast<uint32_t*>(data), reinterpret_cast<uint32_t*>(data) + n_vox, converted.begin());
+    else if (dtype == NIFTI_TYPE_INT64)
+        std::copy(reinterpret_cast<int64_t*>(data), reinterpret_cast<int64_t*>(data) + n_vox, converted.begin());
+    else if (dtype == NIFTI_TYPE_UINT64)
+        std::copy(reinterpret_cast<uint64_t*>(data), reinterpret_cast<uint64_t*>(data) + n_vox, converted.begin());
+    else 
+        throw std::runtime_error("reading failed, unsupported datatype");
+
+
     auto tensor = std::make_unique<Tensor<float>>(TensorShape{dims});
     tensor->from_vector(converted);
     return tensor;
