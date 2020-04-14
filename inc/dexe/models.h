d67beac22008dc466d28e8349a27403911dc8fc2
Marijn Stollenga <mstollenga@gmail.com>
2020-04-14 14:13:17 +0200

    Add two api exports

--- inc/dexe/models.h
+++ inc/dexe/models.h
@@ -4,9 +4,9 @@
 #include <functional>
 
 namespace dexe {
-    template <typename F>
+    template <typename F> DEXE_API
         std::function<Node<F>(Node<F>)> basic_layer(int c, int c_out, int k);
     
-    template <typename F>
+    template <typename F> DEXE_API
         Node<F> make_unet(Network<F> *network, int in_channels, int out_channels, bool local_normalization = false);
 }
